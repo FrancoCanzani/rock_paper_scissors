ec7abbabd0c0175cf834f669ad40e092466a33de
FrancoCanzani <francocanzani@gmail.com>
2022-11-10 20:00:03 +0100

    Main function is working

--- file.js
+++ file.js
@@ -7,3 +7,22 @@ function getComputerChoice() {
   return values[index];
 }
 
+let computerChoice = getComputerChoice();
+let userChoice = (prompt("Please select rock, paper or scissors."));
+userChoice = userChoice.toLowerCase();
+
+/* Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation). */
+
+function roundOfGame(userChoice, computerChoice) {
+  
+  if (userChoice === computerChoice) {
+    return("It is a tie");
+  } else if ((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice == "rock") || (userChoice === "scissors" && computerChoice === "paper")) {
+    return("Player wins!")
+  } else {
+    return("Computer wins!")
+  }
+}
+
+console.log(roundOfGame(userChoice, computerChoice));
