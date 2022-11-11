6779106610eebd4cbba887b4023c1cbdde367f1a
FrancoCanzani <francocanzani@gmail.com>
2022-11-11 22:47:20 +0100

    Comments added

--- file.js
+++ file.js
@@ -1,26 +1,29 @@
+// Setting the game score to 0
 let userScore = 0;
 let computerScore = 0;
 
+// Main game function
+
 function game() {
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) { /* Play the game 5 rounds */
 
-    let values = ["rock", "paper", "scissors"];
-    let index = Math.floor(Math.random() * values.length);
+    let values = ["rock", "paper", "scissors"]; /* The possibilities the computer can choose */
+    let index = Math.floor(Math.random() * values.length); /* I use the random built in function to randomly pick a value from the array */
 
-    function getComputerChoice() {
+    function getComputerChoice() { /* Function for the computer choice */
       return values[index];
     }
 
     let computerChoice = getComputerChoice();
-    let userChoice = (prompt("Please select rock, paper or scissors."));
+    let userChoice = (prompt("Please select rock, paper or scissor.")); /* Prompt so we can enter our choice */
     userChoice = userChoice.toLowerCase();
 
     function roundOfGame(userChoice, computerChoice) {
   
       if ((userChoice !== 'rock') && (userChoice !== 'paper') && (userChoice !== 'scissor')) {
         i-=1;
-        return(`What's "${userChoice}"? Please enter a valid input.`);
+        return(`What's "${userChoice}"? Please enter a valid input.`); /* Return in case we have a TypeError */
       }
       else if (userChoice === computerChoice) {
         return("It is a tie");
