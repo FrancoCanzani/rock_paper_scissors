772f3ed586e7ee14a98c123f52d820c72612341e
Franco Canzani <francocanzani@gmail.com>
2022-11-20 22:12:17 +0100

    Play again button added

--- file.js
+++ file.js
@@ -1,16 +1,25 @@
-// Setting the game score to 0
-let userScore = 0;
-let computerScore = 0;
-
-
 // Access the HTML buttons
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 const player = document.querySelector('.player');
 const computer = document.querySelector('.computer');
+const buttons = document.querySelectorAll('.rock, .paper, .scissors');
+const scanner = document.querySelector('.scanner')
+const resetBtn = document.querySelector('.playAgain')
 
+// Setting the game score to 0
+let userScore = 0;
+let computerScore = 0;
 
+player.innerText = userScore;
+computer.innerText = computerScore;
+
+function disableButtons() {
+  buttons.forEach(elem => {
+      elem.disabled = true
+  })
+}
 
 // Main game function
 
@@ -25,14 +34,26 @@ function game() {
 
   let computerChoice = getComputerChoice();
 
+
   function roundOfGame(userChoice, computerChoice) {
 
     if (userChoice === computerChoice) {
-    
+      scanner.innerText = "It is a tie!"
     } else if ((userChoice === "rock" && computerChoice === "scissors") || (userChoice === "paper" && computerChoice == "rock") || (userChoice === "scissors" && computerChoice === "paper")) {
-    player.innerText = userScore+=1;
+      player.innerText = userScore+=1;
+      scanner.innerText = "You win the round!"
+      if (userScore == 5) {
+        disableButtons()
+        scanner.innerText = "You win the game 🥳!"
+      }
     } else {
     computer.innerText = computerScore+=1;
+    scanner.innerText = "You lose the round!"
+    if (computerScore == 5) {
+      disableButtons()
+      scanner.innerText = "You lost the game 🤦‍♂️!"
+
+  }
     }
   }
 
@@ -52,3 +73,5 @@ paper.addEventListener("click", function(){
 scissors.addEventListener("click", function(){
   return(userChoice = 'scissors', game());
 });
+
+resetBtn.addEventListener('click',() => location.reload());
